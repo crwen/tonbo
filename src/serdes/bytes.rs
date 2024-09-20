53a57772c42da1451494c408e30ddd6741fb9413
crwen <1543720935@qq.com>
2024-09-20 18:19:45 +0800

    fix bytes encode/decode

--- src/serdes/bytes.rs
+++ src/serdes/bytes.rs
@@ -9,6 +9,7 @@ impl Encode for &[u8] {
     type Error = io::Error;
 
     async fn encode<W: AsyncWrite + Unpin>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        writer.write_all(&(self.len() as u16).to_le_bytes()).await?;
         writer.write_all(self).await
     }
 
@@ -21,6 +22,7 @@ impl Encode for Bytes {
     type Error = io::Error;
 
     async fn encode<W: AsyncWrite + Unpin>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        writer.write_all(&(self.len() as u16).to_le_bytes()).await?;
         writer.write_all(self).await
     }
 
@@ -33,7 +35,12 @@ impl Decode for Bytes {
     type Error = io::Error;
 
     async fn decode<R: AsyncRead + Unpin>(reader: &mut R) -> Result<Self, Self::Error> {
-        let mut buf = Vec::new();
+        let len = {
+            let mut len = [0; size_of::<u16>()];
+            reader.read_exact(&mut len).await?;
+            u16::from_le_bytes(len) as usize
+        };
+        let mut buf = vec![0; len];
         reader.read_exact(&mut buf).await?;
 
         Ok(Bytes::from(buf))
